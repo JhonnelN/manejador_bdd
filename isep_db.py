eb1cbd8f8801c021ff9a3a62c517be2fda357166
JhonnelN <45217131+JhonnelN@users.noreply.github.com>
2021-03-05 17:40:40 -0400

    Nuevo modulos agregados

--- isep_db.py
+++ isep_db.py
@@ -12,11 +12,12 @@ def crear_db(nombre):
     mydb = mysql.connector.connect(host=host, user=user, password=password)
     mycursor = mydb.cursor()
     mycursor.execute("CREATE DATABASE " + nombre)
-
+    
 # conectar a bd
 def conectar_db():
     mydb = mysql.connector.connect(host=host, user=user, password=password, database=db)
     mycursor = mydb.cursor()
+    return mydb
 
 # scrip crear tablas
 def crear_tablas():
@@ -35,5 +36,31 @@ def crear_tablas():
     mycursor.execute( "alter table leads add foreign key (id_usuario) references usuarios(id)")
     mycursor.execute( "alter table leads add foreign key (id_ventas) references equipos_ventas(id)")
 
-class Leads:
-    def cargar(nombre, telefono, correo, direccion, id_user, id_ventas)
+#usuario:
+def crear_usuario(nombre, telefono, correo, direccion):
+    mydb = conectar_db()
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO usuarios (nombre, telefono, correo, address) VALUES (%s, %s,%s, %s)"
+    val = (nombre, telefono, correo, direccion)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+def eliminar_usuario(id):
+    mydb = conectar_db()
+    mycursor = mydb.cursor()
+    mycursor.execute("DELETE FROM usuarios WHERE id ="+repr(id)) # la funcion repr() permite concadenar int con str
+    mydb.commit()
+
+def mod_usuario(id, nombre, telefono, correo, direccion):
+    mydb = conectar_db()
+    mycursor = mydb.cursor()
+    #val = ("nombre=" + nombre + ", telefono=" + telefono + ", correo=" + correo + ", address= " + direccion)
+    sql = ("UPDATE usuarios SET nombre = %s, telefono = %s, correo = %s, address = %s WHERE id= %s")
+    mycursor.execute(sql, (nombre, telefono, correo, direccion, id))
+    mydb.commit()
+
+#mod_usuario(3, "Jhonnel", "0424", "mail@mail.com", "direccion")
+#crear_usuario("Jhon2", "0424", "mail@mail.com", "direccion")
+#eliminar_usuario(2)
+#class Leads:
+#    def cargar(nombre, telefono, correo, direccion, id_user, id_ventas)
